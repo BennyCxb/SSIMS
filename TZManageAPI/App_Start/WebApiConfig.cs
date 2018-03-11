7afde106cebdeab1d43d70c115d733c0176c6134
liaoyustudent <932125208@qq.com>
2018-03-11 22:09:21 +0800

    liaoyu
    api修改

--- TZManageAPI/App_Start/WebApiConfig.cs
+++ TZManageAPI/App_Start/WebApiConfig.cs
@@ -13,7 +13,7 @@ namespace TZManageAPI
             //config.MapHttpAttributeRoutes();
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
 
